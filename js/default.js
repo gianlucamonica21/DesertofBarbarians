09e5042a8b9d3952494ce556971d9d4c7fe03339
Gianluca Monica <gianluca.monica@studenti.unipr.it>
2016-12-27 12:57:40 +0100

    Added game in index, updated default.ks like you do

--- js/default.js
+++ js/default.js
@@ -6,10 +6,14 @@ $(document).ready(function(){
 	});
 
 	$('#submitButton').click(function(){
-		var code = $("#editor")[0];
 		editor.save();
-		var data_url = "data:text/javascript;charset=utf-8;base64," + $.base64.encode(code);
-		document.getElementById("result").src = data_url;
+		alert("CLICKED");
+		//var data_url = "data:text/javascript;charset=utf-8;base64," + $.base64.encode(code);
+		//document.getElementById("result").src = data_url;
+		var resultFrame = document.getElementById("result");
+      	var iWind = resultFrame.contentWindow;   // the iframe's window
+     	resultFrame = iWind.eval(code);
+
 	});
 
 });
