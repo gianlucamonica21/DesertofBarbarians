0f14e9144191b166199b72057ade48e3a4f0d31a
Gianluca Monica <gianluca.monica@studenti.unipr.it>
2016-12-28 17:45:57 +0100

    Added prova.js to read the code from file, the submit button works on the 2nd click, on the first the resultFrame appear empty

--- js/default.js
+++ js/default.js
@@ -1,23 +1,53 @@
-$(document).ready(function(){
+	$(document).ready(function(){
 	var code = $("#editor")[0];
 	var editor = CodeMirror.fromTextArea(code, {
 		lineNumbers : true,
 		mode : "javascript"
 	});
 
+	function readTextFile(file)
+	{
+	    var rawFile = new XMLHttpRequest();
+	    rawFile.open("GET", file);
+	    rawFile.onreadystatechange = function ()
+	    {
+	        if(rawFile.readyState === 4)
+	        {
+	            if(rawFile.status === 200 || rawFile.status == 0)
+	            {
+	                var allText = rawFile.responseText;
+	                editor.setValue(allText);
+	                editor.save();
+	                var code = document.getElementById("editor").value;	
+					console.log("code: " + code);	
+					var resultFrame = document.getElementById("result");
+				 	var iWind = resultFrame.contentWindow;   // the iframe's window
+				 	resultFrame = iWind.eval(code);
+	            }
+	        }
+	    }
+	    rawFile.send(null);
+	}
+
+		
+
+	readTextFile("js/prova.js");
+	
 	$('#submitButton').click(function(){
 
-        var code = document.getElementById("editor").value;
 		editor.save();
-		alert("CLICKED");
-		//var data_url = "data:text/javascript;charset=utf-8;base64," + $.base64.encode(code);
-		//document.getElementById("result").src = data_url;
+
+  		var code = document.getElementById("editor").value;
 		var resultFrame = document.getElementById("result");
-      	var iWind = resultFrame.contentWindow;   // the iframe's window
+		var iWind = resultFrame.contentWindow;   // the iframe's window
+     	console.log("iWind " + iWind);
+     	console.log("ecco " + resultFrame.contentWindow.location.reload());
+     	resultFrame.contentWindow.location.reload();
      	resultFrame = iWind.eval(code);
-
-
+     
+   
 	});
+		
 
 });
 
---
+++ js/prova.js
@@ -0,0 +1,1 @@
+document.write("You are reading prova.js!!");
