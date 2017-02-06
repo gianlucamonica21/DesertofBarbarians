48b41065248fdee55d8d9dc190968b3d5dc0739d
Margherita Donnici <margheritadonnici@gmail.com>
2017-02-06 10:29:37 +0100

    created js file for editor code

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 
   $current_player = $_SESSION['loggedinUser'];  
 } else {
-      header("location: DBConnection/loginPage.php");
+  header("location: DBConnection/loginPage.php");
 }
 
 
@@ -419,94 +419,7 @@ $conn = null;
   </footer>
 
 </div>
-
-<script type="text/javascript">
-
-  var stoppedGame = false;
-  // set editor by CodeMirror function
-  var editor = 
-  CodeMirror.fromTextArea(document.getElementById("editor"),{
-    mode: "javascript",
-    lineNumbers: true,
-    styleSelectedText: true
-  });
-  
-  $.ajax({
-    url: 'getLevel.php',
-    type: 'GET',
-    success: function(result){
-      loadLevelJs(result);
-    }
-  });
-
-  var startedCoding;
-  editor.on('beforeChange',function(cm,change) {
-    // if ( ~readOnlyLines.indexOf(change.from.line) ) {
-    //   change.cancel();
-    // }
-    // alert("stai scrivendo");
-    drawStopMessage();
-    startedCoding = (new Date()).getTime();
-    //console.log("Hai iniziato a scrivere :" + startCoding);
-    stoppedGame = true;
-  });
-
-  //Code to reload and reupdate the level  
-  var stringa;
-    var oReq = new XMLHttpRequest(); //New request object
-    oReq.onload = function() {
-    //This is where you handle what to do with the response.
-    //The actual data is found on this.responseText
-    stringa = this.responseText; //Will alert: 42
-  };
-  oReq.open("get", "DBConnection/load_level.php", false);
-    //                               ^ block the rest of the execution.
-    //                                 Don't wait until the request finishes to 
-    //                                 continue.
-    oReq.send(); 
-    alert("RISULTATO  CHIAMATA da index :" + stringa);
-
-    function loadLevelJs(path){
-      var xhr = new XMLHttpRequest();
-      var widgets = [];
-
-      for (var i = 0; i < widgets.length; ++i)
-        window.editor.removeLineWidget(widgets[i]);
-
-      widgets.length = 0;
-
-      xhr.open("GET", path, true);
-      xhr.onload = function (e) {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            //Read the file content and set in the editor
-            editor.setValue(xhr.responseText);
-            //Check errors present in the content of the editor
-            JSHINT(editor.getValue());
-
-            // insert of the error comment in the editor at the right line
-            for (var i = 0; i < JSHINT.errors.length; ++i) {
-              var err = JSHINT.errors[i];
-              if (!err) continue;
-              var msg = document.createElement("div");
-              var icon = msg.appendChild(document.createElement("span"));
-              icon.innerHTML = "*---->";
-              icon.className = "lint-error-icon";
-              msg.appendChild(document.createTextNode(err.reason));
-              msg.className = "lint-error";
-              alert("errore di sintassi");
-              widgets.push(window.editor.addLineWidget(err.line - 1, msg, {coverGutter: false, noHScroll: true})); 
-            }
-          } else {
-            console.error(xhr.statusText);
-          }
-        }
-      };
-      xhr.onerror = function (e) {
-        console.error(xhr.statusText);
-      };
-      xhr.send(null);      
-    }
-  </script>
+<script type="text/javascript" src="js/editor.js"></script>
+<noscript>You need to turn JavaScript on.</noscript>
 </body>
 </html>
