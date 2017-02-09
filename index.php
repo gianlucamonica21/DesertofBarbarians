8bb4c725fbbdaabd260271227e08c57d6b8695e4
Gianluca Monica <gianluca.monica@studenti.unipr.it>
2017-02-09 16:06:12 +0100

    FIXED Static LEVEL LOAD

--- index.php
+++ index.php
@@ -292,7 +292,22 @@ $conn = null;
           </div>
         </div>
         <script type="text/javascript">
+        var levelArr = document.getElementsByClassName("level-buttons");
+        
+        var maxlevel = '<?php echo $_SESSION["maxLevel"]; ?>';
+        for(var i=0; i<levelArr.length; i++)
+        {
+          if(i >= maxlevel){
+            levelArr[i].classList.add("disabled");
+          }
+        } 
           $('.level-buttons').click(function(){
+              
+             //alert(levelArr);
+          //
+          
+
+
             if(!($(this).hasClass("disabled"))){
               var clicked = true;
               clickedLevel = this.textContent;
@@ -314,15 +329,7 @@ $conn = null;
           // oReq.send(data);
           
 
-          var levelArr = document.getElementsByClassName("level-buttons");
-          //alert(levelArr);
-          //
-          // for(var i=0; i<levelArr.length; i++)
-          // {
-          //   if(i >= maxlevel){
-          //     levelArr[i].classList.add("disabled");
-          //   }
-          // }
+          
         </script>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
